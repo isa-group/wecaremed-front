c35904b680734d0643801ad766be9705ad5395e0
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2022-01-31 18:29:05 +0100

    Login and Register with Passport and validations implemented

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,32 @@
   <Toast />
 
   <div class="grid" style="justify-content: center;">
-    <div class="col-3">
+    <div class="col-4">
 			<div class="card">
-				<h2>Login</h2>
+				<h2>Login/Register</h2>
 
 				<div class="p-fluid formgrid grid">
 					<div class="field col-12 md:col-10">
-						<label for="usernameId">Username</label>
-            <InputText v-model="username" name="username" id="usernamedId" @keypress.enter="signIn()" />
+						<label for="usernameId">Email</label>
+            <InputText v-model="username" name="username" id="usernamedId" @keypress.enter="signIn()"
+            :class="{'p-invalid': submitted && error.field === 'email'}" />
+            <small class="p-error" v-if="submitted && error.field === 'email'" :key="error.message">{{error.message}}</small>
 					</div>
         </div>
         <div class="p-fluid formgrid grid">
           <div class="field col-12 md:col-10">
 						<label for="usernameId">Password</label>
-            <Password v-model="password" name="password" id="passwordId" :feedback="false" @keypress.enter="signIn()" />
+            <Password v-model="password" name="password" id="passwordId" :feedback="false" @keypress.enter="signIn()" 
+            :class="{'p-invalid': submitted && error.field === 'password'}" />
+            <small class="p-error" v-if="submitted && error.field === 'password'">{{error.message}}</small>
 					</div>
         </div>
+
+        <small class="p-error" v-if="submitted && error.message === 'Missing credentials'">{{error.message}}</small>
+
         <div class="grid">
-          <Button type="button" icon="pi pi-check" label="Sign in" class="p-button-info mt-4" @click="signIn()" />
+          <Button type="button" icon="pi pi-check" label="Log in" class="p-button-info mt-4 mr-2" @click="signIn()" />
+          <Button type="button" icon="pi pi-check" label="Register" class="p-button-info mt-4 ml-2" @click="signUp()" />
         </div>
       </div>
     </div>
@@ -43,35 +51,42 @@ export default {
     Toast,
   },
   created(){
-    this.signIn();
+    // this.signIn();
   },
   data() {
     return {
-        username: this.$store.state.username,
-        password: this.$store.state.password
+      error: {},
+      submitted: false,
+      username: this.$store.state.username,
+      password: this.$store.state.password
     }
   },
   methods: {
     signIn() {
+      this.submitted = true
       this.$store.dispatch("saveUsername", this.username);
       this.$store.dispatch("savePassword", this.password);
       
-        axios.get('/projects', {
-          auth : {
-              username: this.$store.state.username,
-              password: this.$store.state.password
-          }
-        })
-        .then(resp => {
-          console.log(resp.data);
-          console.log(this.$store.state.username);
-          window.location.href = '/';
-          this.$toast.add({severity:'success', summary: 'Successful', detail: 'Logged in successfully', life: 3000});
-        }).catch(err => {
-          console.log(err);
-          if (this.username !== "" && this.password !== "")
-            this.$toast.add({severity:'error', summary: 'Error', detail: 'Incorrect user and password', life: 3000});
-        })    
+      axios.post('/auth/login', {email: this.username, password: this.password})
+      .then(() => {
+        window.location.href = '/';
+        this.$toast.add({severity:'success', summary: 'Successful', detail: 'Logged in successfully', life: 3000});
+      }).catch(err => {
+        this.error = err.response.data
+      })    
+    },
+    signUp() {
+      this.submitted = true
+      this.$store.dispatch("saveUsername", this.username);
+      this.$store.dispatch("savePassword", this.password);
+      
+      axios.post('/auth/register', {email: this.username, password: this.password})
+      .then(() => {
+        window.location.href = '/';
+        this.$toast.add({severity:'success', summary: 'Successful', detail: 'Registered successfully', life: 3000});
+      }).catch(err => {
+        this.error = err.response.data
+      })    
     }
   }
 }
